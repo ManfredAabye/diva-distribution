c468c793dc8f7b09451557d98b43026b1ccf087f
ManfredAabye <zainhofer@gmx.de>
2024-07-23 22:18:20 +0200

    DOTNET8

--- addon-modules/21Wifi/Localization/make_languages.sh
+++ addon-modules/21Wifi/Localization/make_languages.sh
@@ -1,10 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 
-Usage()
-{
+Usage() {
     cat <<EOT
 
-Syntax: `basename $0` [-o <pathToOpenSimDir>] [-p] [[<langCode>] ...]
+Syntax: $(basename "$0") [-o <pathToOpenSimDir>] [-p] [[<langCode>] ...]
 
 Generate satellite assemblies for the specified languages from resource files
 named Diva.Wifi.<langCode>.resx.
@@ -13,21 +12,19 @@ If option -p is present, then generate from PO files Diva.Wifi.<langCode>.po ins
 EOT
 }
 
-ConvertPO()
-{
+ConvertPO() {
     lang=$1
-    /cygdrive/c/Program\ Files\ \(x86\)/Microsoft\ SDKs/Windows/v10.0A/bin/NETFX\ 4.8\ Tools/ResGen.exe Diva.Wifi.$lang.po Diva.Wifi.$lang.resx
+    po2resx Diva.Wifi.$lang.po Diva.Wifi.$lang.resx
 }
 
-CreateSatelliteAssembly()
-{
+CreateSatelliteAssembly() {
     lang=$1
     dest=$2
-    if /cygdrive/c/Program\ Files\ \(x86\)/Microsoft\ SDKs/Windows/v10.0A/bin/NETFX\ 4.8\ Tools/ResGen.exe Diva.Wifi.$lang.resx Diva.Wifi.$lang.resources; then
-        mkdir $dest/$lang 2> /dev/null
-        /cygdrive/c/Program\ Files\ \(x86\)/Microsoft\ SDKs/Windows/v10.0A/bin/NETFX\ 4.8\ Tools/al.exe /target:library /culture:$lang \
+    if resgen Diva.Wifi.$lang.resx Diva.Wifi.$lang.resources; then
+        mkdir -p "$dest/$lang"
+        al /target:library /culture:$lang \
             /embed:Diva.Wifi.$lang.resources \
-            /out:$dest/$lang/Diva.Wifi.resources.dll
+            /out:"$dest/$lang/Diva.Wifi.resources.dll"
         rm -f Diva.Wifi.$lang.resources
     fi
 }
@@ -35,7 +32,7 @@ CreateSatelliteAssembly()
 # Default parameters
 convertOnly=false
 convertFrom=resx
-osbin=`dirname $0`/../../../bin
+osbin=$(dirname "$0")/../../../bin
 
 # Read command arguments
 while getopts :co:p arg; do
@@ -46,11 +43,11 @@ while getopts :co:p arg; do
     \?) Usage; exit 1;;
     esac
 done
-shift `expr $OPTIND - 1`
+shift $((OPTIND - 1))
 languages=$*
 
 # Check path to bin directory
-if [ ! -f $osbin/Diva.Wifi.dll ]; then
+if [ ! -f "$osbin/Diva.Wifi.dll" ]; then
     echo "Please specify the path to OpenSimulator's bin/ directory with parameter -o"
     exit 2
 fi
@@ -66,8 +63,7 @@ fi
 
 # Create satellite assemblies
 for language in $languages; do
-    $convertOnly || echo Creating satellite assembly for language: $language
+    $convertOnly || echo "Creating satellite assembly for language: $language"
     test "$convertFrom" = "po" && ConvertPO $language
     $convertOnly || CreateSatelliteAssembly $language $osbin
 done
-
