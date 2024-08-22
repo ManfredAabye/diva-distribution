1d8d8e0176f4d9b00cc1f5116ed29dc62775813f
ManfredAabye <zainhofer@gmx.de>
2024-08-22 14:06:18 +0200

    Update runprebuild.sh

--- runprebuild.sh
+++ runprebuild.sh
@@ -3,14 +3,14 @@
 case "$1" in
 
  'clean')
-    dotnet bin/prebuild.dll /file prebuild.xml /clean
+    dotnet bin/prebuild.dll /file divaprebuild.xml /clean
 
   ;;
 
 
   'autoclean')
 
-    echo y|dotnet bin/prebuild.dll /file prebuild.xml /clean
+    echo y|dotnet bin/prebuild.dll /file divaprebuild.xml /clean
 
   ;;
 
@@ -19,9 +19,9 @@ case "$1" in
   *)
 
     cp bin/System.Drawing.Common.dll.linux bin/System.Drawing.Common.dll
-    dotnet bin/prebuild.dll /target vs2022 /targetframework net8_0 /excludedir = "obj | bin" /file prebuild.xml
-    echo "dotnet build -c Release OpenSim.sln" > compile.sh
-    chmod +x compile.sh
+    dotnet bin/prebuild.dll /target vs2022 /targetframework net8_0 /excludedir = "obj | bin" /file divaprebuild.xml
+    echo "dotnet build -c Release DivaOpenSim.sln" > divacompile.sh
+    chmod +x divacompile.sh
 
   ;;
 
