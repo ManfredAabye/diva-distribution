ef4c0cdcf5b427170d29f2f601f41f0d89f2bc8a
ManfredAabye <zainhofer@gmx.de>
2025-11-05 09:34:04 +0100

    05112025933

---
+++ os-start.bat
@@ -0,0 +1,10 @@
+@echo off
+
+echo ====================================
+echo OpenSimulator Diva Distribution
+echo .NET 8 Edition
+echo ====================================
+echo.
+
+cd bin
+dotnet OpenSim.dll
